b03758df8782aece24492834ad33b9ba162bb043
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-04-23 20:58:02 +0900

    3장 입출력 실습

--- C/C 복습/입출력 - 반올림.c
+++ C/C 복습/입출력 - 반올림.c
@@ -2,8 +2,8 @@
 
 int main(void) {
   double num;
-  printf("실수 입력 : ");
+  printf("실수 입력: ");
   scanf("%lf", &num);
-  printf("반올림 정수 출력 : %d", (int)(num+0.5));
+  printf("반올림 정수 출력: %d", (int)(num+0.5));
   return 0;
 }
