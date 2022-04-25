62eaa2363df8058959456151b7c2392ab40faa12
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-04-25 14:54:53 +0900

    5장 조건문 실습

---
+++ C/C 복습/조건문 - 윤년 평년.c
@@ -0,0 +1,13 @@
+int main(void) {
+  int N, M;
+  scanf("%d", &N);
+  if (N%400==0) 
+    printf("leap year");
+  else if (N%100==0)
+    printf("common year");
+  else if (N%4==0)
+    printf("leap year");
+  else
+    printf("common year");
+  return 0;
+}  
