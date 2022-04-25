99b196f5f4aec76000f6bae8c37cb979a8007c35
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-04-25 14:13:32 +0900

    5장 조건문 실습

---
+++ C/C 복습/조건문 - 대소문자 바꾸기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  char alp;
+  scanf("%c", &alp);
+  if (alp>=97 && alp<=122)
+    printf("%c", alp-32);
+  else if (alp>=65 && alp<=90)
+    printf("%c", alp+32);
+  else
+    printf("none");
+}
