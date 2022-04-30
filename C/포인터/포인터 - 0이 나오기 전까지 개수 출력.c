32fb85d4d49ee5e59e0654a8b96ed77524defe5f
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-04-30 18:32:15 +0900

    포인터 실습

---
+++ C/포인터/포인터 - 0이 나오기 전까지 개수 출력.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+  int ar[50]={0}, N, cnt=0;
+  int *p;
+  scanf("%d", &N);
+  for (p=ar; p<ar+N; p++)  # 반복문으로 배열 훑어보기
+    scanf("%d", p);
+  for (p=ar; p<ar+N; p++) {
+    if(*p==0) break;
+    else cnt++;
+  }
+  printf("%d", cnt);
+  return 0;
+}
