8675771ffba4f904b64fdc929ba198b97ff2d253
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-04-30 17:18:41 +0900

    포인터 실습

---
+++ C/포인터/포인터 - 주소 swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+  int x, y, z;
+  int *px, *py, *pz, *tmp;
+  px = &x;
+  py = &y;
+  pz = &z;
+  tmp = NULL;
+  scanf("%d %d %d", px, py, pz);
+  tmp = pz;
+  pz = py;
+  py = px;
+  px = tmp;
+  printf("%d %d %d", *px, *py, *pz);
+  
+  return 0;
+}
