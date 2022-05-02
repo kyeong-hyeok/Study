2e5eb5f0d02b3a29b2cde9af2491b8beaa64ee4b
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-05-02 14:13:40 +0900

    포인터 실습

--- C/포인터/포인터 - 중앙값 출력.c
+++ C/포인터/포인터 - 중앙값 출력.c
@@ -2,14 +2,16 @@
 
 int main(void) {
   int x[3];
-  int *p, tmp;
+  int *p, *k, tmp;
   for(p=x; p<x+3; p++)
     scanf("%d", p);
-  for(p=x; p<x+2; p++) {
-    if(*p>*(p+1)) {
-      tmp = *p;
-      *p = *(p+1);
-      *(p+1) = tmp;
+  for(p=x; p<x+3; p++) {
+    for(k=x; k<x+2; k++) {
+      if(*k>*(k+1)) {
+        tmp = *k;
+        *k = *(k+1);
+        *(k+1) = tmp;
+      }
     }
   }
   printf("%d", *(x+1));
