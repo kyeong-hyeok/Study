b6957da24a898e36429c432c7350e41bbf95c8de
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-05-12 13:29:18 +0900

    구조체 실습

---
+++ C/구조체/구조체 - 복소수 합 출력 함수.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct complex {
+  double real, imag;
+};
+
+struct complex add_complex(struct complex a, struct complex b) {
+  struct complex c;
+  c.real = a.real + b.real;
+  c.imag = a.imag + b.imag;
+  return c;
+}
+int main(void) {
+  struct complex x,y,z;
+  scanf("%lf %lf", &x.real, &x.imag);
+  scanf("%lf %lf", &y.real, &y.imag);
+  z = add_complex(x,y);
+  printf("%.1f + %.1fi", z.real, z.imag);
+  return 0;
+}
