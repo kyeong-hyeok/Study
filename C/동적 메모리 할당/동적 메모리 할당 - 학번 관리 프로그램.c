ae3d029eab4b397abebc73bd0165b9538db775ef
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-05-14 19:12:27 +0900

    동적 메모리 할당 실습

---
+++ C/동적 메모리 할당/동적 메모리 할당 - 학번 관리 프로그램.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  int n,i,d;
+  int *p = NULL;
+  scanf("%d", &n);
+  p = (int *)malloc(n*sizeof(int));
+  for(i=0; i<n; i++) 
+    scanf("%d", &p[i]);
+  scanf("%d", &d);
+  p = (int *)realloc(p,(n-d)*sizeof(int));
+  for(i=0;i<n-d;i++)
+    printf("%d", p[i]);
+  free(p);
+  return 0;
+}
