f07b26af0f02b758cbe9ebf0ab7c20897798f5fe
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-05-22 13:33:32 +0900

    연산자,함수,자료형 실습

---
+++ C/연산자,함수,자료형 고급/고급 문법 - 피보나치 수열.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int fibonacci(int n) {
+  if (n>1)
+    return fibonacci(n-1) + fibonacci(n-2);
+  else if (n==1)
+    return 1;
+  else if (n==0)
+    return 0;
+}
+int main(void) {
+  int n;
+  scanf("%d", &n);
+  printf("%d", fibonacci(n));
+  return 0;
+}
