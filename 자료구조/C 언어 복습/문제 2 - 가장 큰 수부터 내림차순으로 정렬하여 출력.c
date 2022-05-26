d935fe3c831a7de576093009956029629c7e0e88
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-05-26 21:00:07 +0900

    C 언어 복습 실습

---
+++ 자료구조/C 언어 복습/문제 2 - 가장 큰 수부터 내림차순으로 정렬하여 출력.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void ABC(int a[], int k) {
+	int i, max = a[k], inx = k, tmp;
+	for (i = k+1; i <= 10; i++) {
+		if (a[i] > max) {
+			max = a[i];
+			inx = i;
+		}
+	}
+	tmp = a[k];
+	a[k] = max;
+	a[inx] = tmp;
+}
+
+int main(void) {
+	int i, a[10], k=0;
+	for (i = 0; i < 10; i++)
+		scanf_s("%d", &a[i]);
+	for (i = 0; i < 9; i++) {
+		ABC(a, k);
+		k++;
+	}
+	for (i = 0; i < 10; i++)
+		printf(" %d", a[i]);
+	return 0;
+}
