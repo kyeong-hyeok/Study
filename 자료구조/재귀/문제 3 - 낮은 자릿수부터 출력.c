d247a56c4053e1ad5707de4bfd806543522030f3
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-06-02 17:02:28 +0900

    재귀 실습

---
+++ 자료구조/재귀/문제 3 - 낮은 자릿수부터 출력.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void digits(int n) {
+	if (n < 10) {
+		printf("%d\n", n);
+		return 0;
+	}
+	else {
+		printf("%d\n", n % 10);
+		digits(n / 10);
+	}
+}
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+	digits(n);
+	return 0;
+}
