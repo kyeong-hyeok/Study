90e84f7e403cad677e2a6b9d2d61812ad4aed767
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-06-02 17:01:07 +0900

    재귀 실습

---
+++ 자료구조/재귀/문제 2 - 높은 자릿수부터 출력.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void digits(int n) {
+	if (n < 10) {
+		printf("%d\n", n);
+		return 0;
+	}
+	else {
+		digits(n / 10);
+		printf("%d\n", n % 10);
+	}
+}
+
+int main(void) {
+	int n;
+	scanf_s("%d", &n);
+	digits(n);
+	return 0;
+}
