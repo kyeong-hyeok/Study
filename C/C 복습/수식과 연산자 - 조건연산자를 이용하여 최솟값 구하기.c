ac0a50f19081f90c5a960d3d200ea6721ee4f545
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-06-23 12:38:44 +0900

    4장 수식과 연산자 실습

--- C/C 복습/수식과 연산자 - 조건연산자를 이용하여 최솟값 구하기.c
+++ C/C 복습/수식과 연산자 - 조건연산자를 이용하여 최솟값 구하기.c
@@ -1,3 +1,6 @@
+#include<stdio.h>
+
+int main() {
 	int n, o, t, h, min;	// 양의 정수 n, 각각 일,십,백의 자릿수, 최솟값 min
 	scanf("%d", &n);	// n 입력받기
 	o = n % 10;	// 일의 자릿수 구하기
@@ -6,3 +9,5 @@
 	min = (o < t) ? o : t;	// o와 t 비교하여 작은 값 min에 저장
 	min = (h < min) ? h : min;	// h와 min 비교하여 작은 값 min에 저장
 	printf("%d", min);	// 최솟값 출력
+	return 0;
+}
