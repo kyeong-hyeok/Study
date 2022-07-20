84c9f8a36444ae663d0938241e005d3665374017
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-07-20 23:11:47 +0900

    ch 3 조건문 연습문제

---
+++ JAVA/JAVA의 정석/조건문 - 각 자리의 합 더한 결과 출력.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class EX {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    int num = Integer.parseInt(input);
+    int sum=0;
+    while(num!=0) {
+        sum += num%10;
+        num/=10;
+    }
+    System.out.println(sum);
+    }
+}
