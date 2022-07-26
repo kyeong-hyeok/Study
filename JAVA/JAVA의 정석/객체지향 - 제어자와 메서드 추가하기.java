a781201d4c5a07de34632c241523cfae8bcc3586
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-07-26 19:57:44 +0900

    ch 7 객체지향 연습문제

---
+++ JAVA/JAVA의 정석/객체지향 - 제어자와 메서드 추가하기.java
@@ -0,0 +1,34 @@
+class MyTv2 {
+    private boolean isPowerOn;
+    private int channel;
+    private int volume;
+    final int MAX_VOLUME = 100;
+    final int MIN_VOLUME = 0;
+    final int MAX_CHANNEL = 100;
+    final int MIN_CHANNEL = 1;
+    void setChannel(int channel) {
+        if(channel>MAX_CHANNEL || channel<MIN_CHANNEL)
+            return;
+        else this.channel=channel;
+    }
+    void setVolume(int volume) {
+        if(volume>MAX_VOLUME || volume<MIN_VOLUME)
+            return;
+        else this.volume=volume;
+    }
+    int getChannel() {
+        return channel;
+    }
+    int getVolume() {
+        return volume;
+    }
+}
+class Exercise7_10 {
+    public static void main(String args[]) {
+        MyTv2 t = new MyTv2();
+        t.setChannel(10);
+        System.out.println("CH:"+t.getChannel());
+        t.setVolume(20);
+        System.out.println("VOL:"+t.getVolume());
+    }
+}
