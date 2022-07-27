f794f7e8a89cf6b960924975cb3bd6a864ecc5c4
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-07-27 13:30:04 +0900

    ch 7 객체지향 연습문제

--- JAVA/JAVA의 정석/객체지향 - 클래스를 정의하고 면적을 구하는 메서드 작성.java
+++ JAVA/JAVA의 정석/객체지향 - 클래스를 정의하고 면적을 구하는 메서드 작성.java
@@ -61,3 +61,15 @@ class Point {
         return "["+x+","+y+"]";
     }
 }
+class Exercise7_23 {
+    public static double sumArea(Shape[] arr) {
+        double sum=0.0;
+        for(int i=0; i<arr.length; i++)
+            sum += arr[i].calcArea();
+        return sum;
+    }
+    public static void main(String[] args) {
+        Shape[] arr = {new Circle(5.0), new Rectangle(3,4), new Circle(1)};
+        System.out.println(" :"+sumArea(arr));
+    }
+}
