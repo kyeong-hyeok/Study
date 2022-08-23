6b141f12a3cd976200f7fe39f2a5c8d76a6711bf
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-08-23 12:59:03 +0900

    ch 11 컬렉션 프레임웍 예제

---
+++ JAVA/JAVA의 정석/HashMap - id, password 일치 확인.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class EX {
+    public static void main(String args[]) {
+       HashMap map = new HashMap();
+       map.put("rudgur", "1234");
+       map.put("alsrud", "1111");
+       System.out.println(map);
+       Scanner s = new Scanner(System.in);
+       while(true) {
+           System.out.println("id와 password를 입력해주세요.");
+           System.out.print("id : ");
+           String id = s.nextLine().trim();
+           System.out.print("password : ");
+           String password = s.nextLine().trim();
+           if(!map.containsKey(id)) {
+               System.out.println("입력하신 id는 존재하지 않습니다. 다시 입력해주세요.");
+               continue;
+           }
+           if(!(map.get(id)).equals(password)) {
+               System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+           } else {
+               System.out.println("id와 비밀번호가 일치합니다.");
+               break;
+           }
+       }
+    }
+}
