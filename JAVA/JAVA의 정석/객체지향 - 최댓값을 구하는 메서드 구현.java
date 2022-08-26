3becc578126308ec9b9d468e7953fb2429415b70
koeyhk <80199502+kyeong-hyeok@users.noreply.github.com>
2022-08-26 18:40:10 +0900

    ch 6 객체지향 연습문제

---
+++ JAVA/JAVA의 정석/객체지향 - 최댓값을 구하는 메서드 구현.java
@@ -0,0 +1,19 @@
+public class EX {
+    public static int max(int[] arr) {
+        if(arr == null || arr.length==0)
+            return -999999;
+        int max=arr[0];
+        for(int i=1; i< arr.length; i++) {
+            if(arr[i]>max) max=arr[i];
+        }
+        return max;
+    }
+    public static void main(String[] args)
+    {
+        int[] data = {3,2,9,4,7};
+        System.out.println(java.util.Arrays.toString(data));
+        System.out.println("최댓값 : "+max(data));
+        System.out.println("최댓값 : "+max(null));
+        System.out.println("최댓값 : "+max(new int[]{}));
+    }
+}
